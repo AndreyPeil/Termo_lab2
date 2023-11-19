7d394b2cb64a357f27bdbf1f361a235b30d4c64f
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-19 17:10:10 -0300

    Update and rename input_validation.py to valid_check.py
    Changed name for easier reading and usage and implemented more validations.

---
+++ valid_check.py
@@ -0,0 +1,16 @@
+def player_input_validation(player_input, words_used, possible_words):
+    alphabet_check = player_input.isalpha()
+    if len(player_input) != 5:
+        print("Word must have 5 letters.")
+        return False
+    elif player_input in words_used:
+        print("Word prevented from being used twice.")
+        return False
+    elif alphabet_check == False:
+        print("Word can only contain letters from the BR Alphabet.")
+        return alphabet_check
+    elif player_input not in possible_words:
+        print("Word not found in game, make sure to not use accentuation marks.")
+        return False
+    else:
+        return True
