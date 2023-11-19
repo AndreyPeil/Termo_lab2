27a1991376b6ac865a30b17e7bdfcfe2b3e51dc8
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-19 17:12:13 -0300

    Update termo_game.py
    Added the start of games and a credits screen that will be called and most likely recursively call main.py

--- termo_game.py
+++ termo_game.py
@@ -1,27 +1,53 @@
 import termo_raffle
-import input_validation
-def solo(words_used,termo_words):
+import valid_check
+def solo(words_used, possible_words, termo_words):
     termo_attempts = 0
     while termo_attempts < 6:
         while True:
-            player_input = input(">> ")
-            check = input_validation.player_input_validation(player_input,words_used)
+            player_input = input(">> ").lower()
+            check = valid_check.player_input_validation(player_input, words_used, possible_words)
             if check == False:
                 pass
             else:
                 break
-def dueto():
-    pass
-def quarteto():
-    pass
+        words_used.append(player_input)
+        termo_attempts += 1
+def dueto(words_used, possible_words, termo_words):
+    termo_attempts = 0
+    while termo_attempts < 7:
+        while True:
+            player_input = input(">> ").lower()
+            check = valid_check.player_input_validation(player_input, words_used, possible_words)
+            if check == False:
+                pass
+            else:
+                break
+        words_used.append(player_input)
+        termo_attempts += 1
+def quarteto(words_used, possible_words, termo_words):
+    termo_attempts = 0
+    while termo_attempts < 9:
+        while True:
+            player_input = input(">> ").lower()
+            check = valid_check.player_input_validation(player_input, words_used, possible_words)
+            if check == False:
+                pass
+            else:
+                break
+        words_used.append(player_input)
+        termo_attempts += 1
+def credits():
+    party_popper = "\U0001F389"
+    applause = "\U0001F44F"
+    print(f"CONGRATULATIONS! {applause} {party_popper}")
 def termo(gamemode):
     words_used = []
-    termo_words = termo_raffle.word_of_the_run(gamemode)
+    termo_words, possible_words = termo_raffle.word_of_the_run(gamemode)
     if gamemode == 1:
-        solo(words_used,termo_words)
+        solo(words_used, possible_words, termo_words)
     elif gamemode == 2:
-        dueto()
+        dueto(words_used, possible_words, termo_words)
     elif gamemode == 3:
-        quarteto()
+        quarteto(words_used, possible_words, termo_words)
     else:
         print("Goodybye, friend, see you another time!")
