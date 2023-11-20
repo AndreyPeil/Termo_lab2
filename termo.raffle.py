76b98cad0be506119e3f3328f24d19bcaad9fa16
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-20 11:44:42 -0300

    Update termo.raffle.py
    Return if gamemode less than 1 and higher than 3. Changes to logic and usage of read().splitlines()

--- termo.raffle.py
+++ termo.raffle.py
@@ -2,18 +2,19 @@ import random
 def word_of_the_run(gamemode):
     termo_words = []
     termo_file = open("palavras_termo.txt", "r")
-    possible_words = termo_file.readlines()
-    # Defina palavra(s) escolhida(s) #
+    possible_words = termo_file.read().splitlines()
     if gamemode == 3:
         draw_times = 4
     elif gamemode == 1 or gamemode == 2:
         draw_times = gamemode
+    else:
+        return False, False
     for i in range(draw_times):
         number_draw = random.randint(0,507)
         chosen_word = possible_words[number_draw]
-        chosen_list = chosen_word.split("\n")
-        if chosen_list[0] in termo_words:
+        if chosen_word in termo_words:
             pass
         else:
-            termo_words.append(chosen_word[0])
+            termo_words.append(chosen_word)
+    termo_file.close()
     return termo_words, possible_words
