c87c689bc7d564bec3610816340d68461b209454
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-20 19:35:00 -0300

    Update termo.raffle.py
    Added the logic that impossibilitates word already played to be played again.

--- termo.raffle.py
+++ termo.raffle.py
@@ -2,19 +2,24 @@ import random
 def word_of_the_run(gamemode):
     termo_words = []
     termo_file = open("palavras_termo.txt", "r")
+    played_file = open("palavras_já_sorteadas.txt", "r")
     possible_words = termo_file.read().splitlines()
+    played_words = played_file.read().splitlines()
     if gamemode == 3:
         draw_times = 4
     elif gamemode == 1 or gamemode == 2:
         draw_times = gamemode
     else:
         return False, False
-    for i in range(draw_times):
+    while len(termo_words) < draw_times:
         number_draw = random.randint(0,507)
         chosen_word = possible_words[number_draw]
         if chosen_word in termo_words:
             pass
+        elif chosen_word in played_words:
+            pass
         else:
             termo_words.append(chosen_word)
     termo_file.close()
+    played_file.close()
     return termo_words, possible_words
