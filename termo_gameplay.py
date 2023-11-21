b2e8440c2be6e810921e05e5a614e637c4f966bb
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-21 12:46:30 -0300

    Update termo_gameplay.py
    Logic corrected, it should now be working properly and hopefully with no errors.

--- termo_gameplay.py
+++ termo_gameplay.py
@@ -1,25 +1,29 @@
-def compare(player_input, termo_words):
+def compare(player_input, word):
     answer_list = ["miss","miss","miss","miss","miss"]
-    termo_list = list(termo_words[0])
+    termo_list = list(word)
     player_input_list = list(player_input) 
     for g in range(5):
         if termo_list[g] == player_input_list[g]:
             answer_list[g] = "green"
             termo_list[g] = ""
+            player_input_list[g] = "0"
     for y in range(5):
-        for w in range (5):
-            if y == w:
-                pass
-            elif player_input_list[y] == termo_list[w]:
-                answer_list[y] = "yellow"
-                termo_list[w] = ""
+        if answer_list[y] == "green":
+            pass
+        else:
+            for w in range(5):
+                if player_input_list[y] == termo_list[w]:
+                    answer_list[y] = "yellow"
+                    termo_list[w] = ""
+                    player_input_list[y] = "0"
+    correct_list = list(player_input)
     for answer in range(5):
         if answer_list[answer] == "green":
-            print(f'\033[32m{player_input_list[answer]} \033[0m', end='|')
+            print(f'\033[32m{correct_list[answer]} \033[0m', end='|')
         elif answer_list[answer] == "yellow":
-            print(f'\033[33m{player_input_list[answer]} \033[0m', end='|')
+            print(f'\033[33m{correct_list[answer]} \033[0m', end='|')
         else:
-            print(f'\033[30m{player_input_list[answer]} \033[0m', end='|')
+            print(f'\033[30m{correct_list[answer]} \033[0m', end='|')
     print()
 def credits(game_win, termo_attempts):
     if game_win == True:
