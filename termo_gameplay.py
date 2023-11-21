802bcbb42ef9e1a69bf2c14e10f05125224fdf15
otaviof3 <126731189+otaviof3@users.noreply.github.com>
2023-11-21 02:19:54 -0300

    Update termo_gameplay.py
    You can now actually play Termo properly with this! Also added credits to the credits, really important stuff you know.

--- termo_gameplay.py
+++ termo_gameplay.py
@@ -1,15 +1,35 @@
+def compare(player_input, termo_words):
+    answer_list = ["miss","miss","miss","miss","miss"]
+    termo_list = list(termo_words[0])
+    player_input_list = list(player_input) 
+    for g in range(5):
+        if termo_list[g] == player_input_list[g]:
+            answer_list[g] = "green"
+            termo_list[g] = ""
+    for y in range(5):
+        for w in range (5):
+            if y == w:
+                pass
+            elif player_input_list[y] == termo_list[w]:
+                answer_list[y] = "yellow"
+                termo_list[w] = ""
+    for answer in range(5):
+        if answer_list[answer] == "green":
+            print(f'\033[32m{player_input_list[answer]} \033[0m', end='|')
+        elif answer_list[answer] == "yellow":
+            print(f'\033[33m{player_input_list[answer]} \033[0m', end='|')
+        else:
+            print(f'\033[30m{player_input_list[answer]} \033[0m', end='|')
+    print()
 def credits(game_win, termo_attempts):
     if game_win == True:
         party_popper = "\U0001F389"
         applause = "\U0001F44F"
         print(f"CONGRATULATIONS! {applause} {party_popper}")
-        play_again = input("Play again? [Press ""Y""]").upper()
-        if play_again == "Y":
-            import main
-            main.main()
+        print(f"Attempts taken to win: {termo_attempts}")
+        print("Group project by Andrey and Otavio.")
+        input("Press enter to play again. ")
     else:
         print("You lose, try again next time.")
-        play_again = input("Play again? [Press ""Y""]").upper()
-        if play_again == "Y":
-            import main
-            main.main()
+        print("Group project by Andrey and Otavio.")
+        input("Press enter to play again. ")
